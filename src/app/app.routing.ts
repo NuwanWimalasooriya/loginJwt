dc0c3e757a99f9113c8ae4370860be094fd26743
NuwanWimalasooriya <NuwanWimalasooriya@users.noreply.github.com>
2019-07-31 09:55:03 +0530

    added
    added

---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { CustomerComponent } from './customer/customer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthGuard } from './_helpers';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    {path: 'customer',component: CustomerComponent},
+    {path: '',redirectTo:'home',pathMatch:'full'},
+    {path:'**',component:PageNotFoundComponent}
+
+    // otherwise redirect to home
+    //{ path: '**', redirectTo: '' }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routes);
